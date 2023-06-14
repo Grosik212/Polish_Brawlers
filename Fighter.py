86bc0680335d35fa35524de4654f75ccfdb0297c
Grosik212 <bartekgrosicki2@gmail.com>
2023-06-15 01:01:24 +0200

    added health bars, classes Player1 and Player2, and made them inherit fom fighter

--- Fighter.py
+++ Fighter.py
@@ -1,12 +1,12 @@
 import pygame
 import game
-
 class Fighter():
     def __init__(self, x, y):
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.attacking = False
         self.jumping = False
+        self.health = 100
 
     def move(self, target):
         speed = 5
@@ -20,8 +20,7 @@ class Fighter():
         if key[pygame.K_d]:
             dx = speed
 
-
-        #stay on screen
+        # Stay on screen
         if self.rect.left + dx < 0:
             dx = -self.rect.left
         if self.rect.right + dx > 800:
@@ -31,21 +30,19 @@ class Fighter():
             self.vel_y = 0
             self.jumping = False
 
-        #jump
+        # Jump
         if self.vel_y == 0 and key[pygame.K_w] and not self.jumping:
             self.vel_y = -30
             self.jumping = True
 
-
-        #apply gravity
+        # Apply gravity
         self.vel_y += gravity
-
         dy += self.vel_y
 
         self.rect.x += dx
         self.rect.y += dy
 
-        #attack
+        # Attack
         if key[pygame.K_SPACE] and not self.attacking:
             self.attack_kick(game.screen, target)
         if key[pygame.K_q] and not self.attacking:
@@ -65,6 +62,58 @@ class Fighter():
         if attacking_rect.colliderect(target.rect):
             target.health -= 5
 
-
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+
+
+class Player1(Fighter):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def draw_health(self, surface):
+        health_width = int(self.rect.width * (self.health / 100))
+        health_rect = pygame.Rect(self.rect.left, self.rect.top - 20, health_width, 10)
+        pygame.draw.rect(surface, (0, 255, 0), health_rect)
+
+
+class Player2(Fighter):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def draw_health(self, surface):
+        health_width = int(self.rect.width * (self.health / 100))
+        health_rect = pygame.Rect(self.rect.left, self.rect.top - 20, health_width, 10)
+        pygame.draw.rect(surface, (0, 255, 0), health_rect)
+
+
+class HealthBar():
+    def __init__(self, x, y, fighter):
+        self.rect = pygame.Rect(x, y, 80, 10)
+        self.fighter = fighter
+
+    def draw(self, surface):
+        self.draw_health(surface)
+
+    def draw_health(self, surface):
+        health_width = int(self.fighter.rect.width * (self.fighter.health / 100))
+        health_rect = pygame.Rect(self.fighter.rect.left, self.fighter.rect.top - 20, health_width, 10)
+        hit_rect = pygame.Rect(self.fighter.rect.left, self.fighter.rect.top - 20, 80, 10)
+        pygame.draw.rect(surface, (255, 0, 0), hit_rect)
+        pygame.draw.rect(surface, (0, 255, 0), health_rect)
+
+
+
+class LargeHealthBar():
+    def __init__(self, x, y, fighter):
+        self.rect = pygame.Rect(x, y, 250, 20)
+        self.fighter = fighter
+
+    def draw(self, surface):
+        self.draw_health(surface)
+
+    def draw_health(self, surface):
+        health_width = int(self.rect.width * (self.fighter.health / 100))
+        health_rect = pygame.Rect(self.rect.left, self.rect.top, health_width, self.rect.height)
+        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        pygame.draw.rect(surface, (0, 255, 0), health_rect)
+
--- game.py
+++ game.py
@@ -11,10 +11,15 @@ background = pygame.image.load("backgrounds/background_bar.png").convert_alpha()
 background = pygame.transform.scale(background, (800, 600))
 
 # creating fighters
-fighter1 = Fighter.Fighter(100, 350)
-fighter2 = Fighter.Fighter(650, 350)
+fighter1 = Fighter.Player1(100, 350)
+fighter2 = Fighter.Player2(650, 350)
+
+health_bar1 = Fighter.HealthBar(100, 380, fighter1)
+health_bar2 = Fighter.HealthBar(600, 380, fighter2)
 
 
+large_health_bar1 = Fighter.LargeHealthBar(50, 50, fighter1)
+large_health_bar2 = Fighter.LargeHealthBar(500, 50, fighter2)
 def game():
     running = True
     while running:
@@ -26,6 +31,11 @@ def game():
         fighter1.draw(screen)
         fighter2.draw(screen)
 
+        health_bar1.draw(screen)
+        health_bar2.draw(screen)
+
+        large_health_bar1.draw(screen)
+        large_health_bar2.draw(screen)
 
         click = False
         for event in pygame.event.get():
